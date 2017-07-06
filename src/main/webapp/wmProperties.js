a468633b9ce6c454dc939a6677f93a2b596736a7
root <root@61949b3d2f95>
2017-07-06 07:33:11 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_LjnEGOyVQt",
+  "homePage" : "Main",
+  "name" : "Auto_LjnEGOyVQt",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
